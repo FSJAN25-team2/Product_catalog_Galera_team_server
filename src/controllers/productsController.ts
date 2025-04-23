bbc8fb46af2bee05b4c5b40f12686bfb82bd5525
vladrlex <v.bondarenko.dev@gmail.com>
2025-04-23 22:08:26 +0300

    feat/products-hot-prices

--- src/controllers/productsController.ts
+++ src/controllers/productsController.ts
@@ -22,24 +22,42 @@ export const getAllProducts = async (req: Request, res: Response) => {
     const startIndex = (currentPage - 1) * itemsPerPage;
     const endIndex = startIndex + itemsPerPage;
 
-    const paginatedProducts = response.slice(startIndex, endIndex);
-
     switch (sortBy) {
       case Sorting.MostExpensive:
-        paginatedProducts.sort((a, b) => b.price - a.price);
+        response.sort((a, b) => b.price - a.price);
         break;
 
       case Sorting.Cheapest:
-        paginatedProducts.sort((a, b) => a.price - b.price);
+        response.sort((a, b) => a.price - b.price);
         break;
 
       case Sorting.Newest:
       default:
-        paginatedProducts.sort((a, b) => b.year - a.year);
+        response.sort((a, b) => b.year - a.year);
         break;
     }
 
+    const paginatedProducts = response.slice(startIndex, endIndex);
+
     res.json(paginatedProducts);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching product" });
+  }
+};
+
+export const getHotPricedProducts = async (req: Request, res: Response) => {
+  try {
+    const { limit = 8 } = req.query;
+    const response = products.map((product) => ({
+      ...product,
+      discount: product.fullPrice - product.price,
+    }));
+
+    response.sort((a, b) => b.discount - a.discount);
+
+    const sliced = response.slice(0, +limit);
+
+    res.json(sliced);
   } catch (error) {
     res.status(500).json({ message: "Error fetching product" });
   }
