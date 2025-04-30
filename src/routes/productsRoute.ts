1ca80876da100a2162ca3c4b432298b5c93a96c4
vladrlex <v.bondarenko.dev@gmail.com>
2025-04-30 03:09:22 +0300

    feat/productById-function

--- src/routes/productsRoute.ts
+++ src/routes/productsRoute.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getAllProducts, getHotPricedProducts } from "../controllers/productsController";
+import { getAllProducts, getHotPricedProducts, getProductById } from "../controllers/productsController";
 
 const router = Router();
 
 router.get("/", getAllProducts);
 router.get("/hot-prices", getHotPricedProducts);
+router.get("/:id", getProductById);
 
 export default router;
